a0b1cc7b9fce4dd6e33e12aad9698886873dea0f
tzuhan <tzuhan@creationline.com>
2011-10-05 15:02:34 +0900

    Added Version.java file to project to track versioning info.  There are also two python scripts that make up the start_commit plugin which will automatically update this Version.java file, but since they reside under bzrlib outside of this repository, they are not sync-ed here.
    Added ".DS_Store" as a bzr ignore pattern.

---
+++ CloudStackAndroidClient/src/com/creationline/cloudstack/util/Version.java
@@ -0,0 +1,16 @@
+package com.creationline.cloudstack.util;
+
+public class Version {
+	// MAJOR_VER and MINOR_VER can be edited by hand.  REVISION_NO and REVISION_ID will be updated
+	// automatically every time the source is committed to Bazaar.
+	public static final int MAJOR_VER = 0;
+	public static final int MINOR_VER = 1;
+	public static final int REVISION_NO = -1;  //this must be on its own line (updated by incrementVersion.py automatically)
+    public static final String REVISION_ID = "x";  //this must be on its own line (updated by incrementVersion.py automatically)
+
+    @Override
+	public String toString() {
+		return MAJOR_VER+"."+MINOR_VER+"."+REVISION_NO+"  ["+REVISION_ID+"]";
+	}
+
+}
