a7900da71a31810f6af5bd185d5b185f201fe31c
tzuhan <tzuhan@creationline.com>
2011-10-26 15:34:23 +0900

    CsSnapshotList changes: - added some comments - removed uncessary imports

--- CloudStackAndroidClient/src/com/creationline/cloudstack/ui/CsSnapshotList.java
+++ CloudStackAndroidClient/src/com/creationline/cloudstack/ui/CsSnapshotList.java
@@ -1,6 +1,5 @@
 package com.creationline.cloudstack.ui;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,8 +14,6 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.ResourceCursorAdapter;
-import android.text.format.Time;
-import android.util.TimeFormatException;
 import android.view.View;
 import android.widget.TextView;
 
@@ -68,18 +65,29 @@ public class CsSnapshotList extends ListFragment implements LoaderManager.Loader
 			}
 		}
 
-		public void setCreatedDateTime(View view, TextView tv, String text) {
+		/**
+		 * Specifically sets the "created" and "createdTime" textviews in the specified view with
+		 * parsed output from passed-in datetimeStr.  Format of datetimeStr is assumed to be fixed.
+		 * In the case datetimeStr cannot be successfully parsed/formated, the textviews will not be set.
+		 * 
+		 * @param view view which contains "created" and "createdTime" textviews to set with date/time text
+		 * @param tv the "created" textview (this is passed in just to save processing since we have this already in the existing code flow)
+		 * @param datetimeStr string in pre-determined format of the datetime to parse
+		 */
+		public void setCreatedDateTime(View view, TextView tv, String datetimeStr) {
 			try {
+				//lazy init & re-use
 				if(datetimeParser==null) {datetimeParser = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");};
 				if(datePrinter==null) {datePrinter = new SimpleDateFormat("yyyy-MM-dd");};
 				if(timePrinter==null) {timePrinter = new SimpleDateFormat("HH:mm:ss");};
 				
-				final Date date = datetimeParser.parse(text);  //parse the date string
+				final Date date = datetimeParser.parse(datetimeStr);  //parse the date string
 				
 				tv.setText(datePrinter.format(date));  //set just the date info
 				
 				tv = (TextView) view.findViewById(R.id.createdtime);
 				tv.setText(timePrinter.format(date));  //set the time info separately from date info
+
 			} catch (ParseException e) {
 				//in the case of an un-parse-able datetime str, we will just display the str as is instead of trying to prettify it
 				ClLog.e("setTextViewWithString():", "created timestamp could not be parsed; skipping");
